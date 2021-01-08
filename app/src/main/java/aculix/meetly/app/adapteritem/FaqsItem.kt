17a40d9f10a8c6e5fa8f7c870894f4568d85f25b
Tommy Maulana <tommmy.maulana@gmail.com>
2021-01-08 16:22:50 +0700

    first commit

---
+++ app/src/main/java/aculix/meetly/app/adapteritem/FaqsItem.kt
@@ -0,0 +1,39 @@
+package aculix.meetly.app.adapteritem
+
+import aculix.meetly.app.R
+import aculix.meetly.app.databinding.ItemFaqsBinding
+import aculix.meetly.app.model.Faq
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.mikepenz.fastadapter.binding.AbstractBindingItem
+
+class FaqsItem(val faq: Faq) :
+    AbstractBindingItem<ItemFaqsBinding>() {
+
+    override val type: Int
+        get() = R.id.fastadapter_faqs_item_id
+
+    override fun createBinding(
+        inflater: LayoutInflater,
+        parent: ViewGroup?
+    ): ItemFaqsBinding {
+        return ItemFaqsBinding.inflate(inflater, parent, false)
+    }
+
+    override fun bindView(binding: ItemFaqsBinding, payloads: List<Any>) {
+        with(binding) {
+            tvTitle.text = faq.title
+            tvDescription.text = faq.description
+        }
+
+    }
+
+    override fun unbindView(binding: ItemFaqsBinding) {
+        with(binding) {
+            tvTitle.text = null
+            tvDescription.text = null
+        }
+    }
+
+
+}
---
+++ app/src/main/java/aculix/meetly/app/model/Faq.kt
@@ -0,0 +1,7 @@
+package aculix.meetly.app.model
+
+data class Faq(
+    val id: Int,
+    val title: String,
+    val description: String
+)
